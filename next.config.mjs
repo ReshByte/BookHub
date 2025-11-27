99aacb5286c21828b8f4f52d6ff42bb6921584a1
ReshByte <redwanreshat@gmail.com>
2025-11-27 09:42:09 +0600

    Last touch

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+ 
   reactCompiler: true,
+  // Remove static export because we need SSR
+  output: undefined,
 };
 
 export default nextConfig;
